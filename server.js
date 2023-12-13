22014896e2aae98d67b557bccdb4135ac487f0ad
Beatriz Andrade <Beatriz1andrade2013@gmail.com>
2023-12-12 22:47:18 -0300

    Update server.js
    correção de título de artigo

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.get('/articles', (req, res) => {
   const articles = [
-	{ id: 1, title: 'Sustentabilidade Ambienta', content: 'Preservar o planeta é nossa responsabilidade coletiva. Adotar energias renováveis e reduzir resíduos são passos vitais para um futuro sustentável' },
+	{ id: 1, title: 'Sustentabilidade Ambiental', content: 'Preservar o planeta é nossa responsabilidade coletiva. Adotar energias renováveis e reduzir resíduos são passos vitais para um futuro sustentável' },
 	{ id: 2, title: 'Responsabilidade Social', content: 'A inclusão e diversidade fortalecem nossas comunidades e empresas. Juntos, podemos criar ambientes mais justos e acolhedores' },
 	{ id: 3, title: 'Governança Corporativa', content: 'Ética nos negócios é a base de uma governança sólida. Transparência e conformidade são pilares para confiança e crescimento sustentável' },
 	{ id: 4, title: 'Tecnologia e Inovação Sustentável', content: 'A tecnologia pode ser a chave para soluções sustentáveis. Inovações inteligentes impulsionam o caminho para um futuro mais verde' },
